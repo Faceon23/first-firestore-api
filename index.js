8b2a9d02a12132db175ec876487f4e76c4a3a036
Roderick Fayson <roderickfayson@Rodericks-MacBook-Air.local>
2022-07-26 14:41:39 -0400

    morestuff

---
+++ index.js
@@ -0,0 +1,14 @@
+import express from 'express';
+
+const app = express();
+const PORT = 3002;
+app.use(express.json());
+
+// put our routes here
+app.get('/',(req, res) => {
+    res.send('😁😁😁Express is working😁😁😁');
+});
+
+app.listen(PORT,() =>{
+    console.log(`Listening on http://localhost:${PORT}/ ...`);
+});
